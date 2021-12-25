330424d2c200ca5fa6d3808bc7af106482013c19
MatejPrasek <prasek.matej@seznam.cz>
2021-12-25 22:39:51 +0100

    Project created

---
+++ ProcrastinationBlocker/ProcrastinationBlocker/Program.cs
@@ -0,0 +1,10 @@
+using ProcrastinationBlocker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
